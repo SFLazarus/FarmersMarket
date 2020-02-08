bc0dd73d81442c4b6b5057c928819e7b5025dbd3
Owen Mellema <owenmllm@gmail.com>
2020-02-08 16:49:27 -0600

    Add customer account editing and customer requesting

---
+++ app/src/main/java/com/lazarus/farmersmarket/CustomerMakeRequest.java
@@ -0,0 +1,14 @@
+package com.lazarus.farmersmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMakeRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_make_request);
+    }
+}
