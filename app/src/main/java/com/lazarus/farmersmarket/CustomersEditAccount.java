a0ba807e953b37bc45ad88b56eb871bb84f799e3
Owen Mellema <owenmllm@gmail.com>
2020-02-09 05:00:40 -0600

    Solve Conflicts

--- app/src/main/java/com/lazarus/farmersmarket/CustomersEditAccount.java
+++ app/src/main/java/com/lazarus/farmersmarket/CustomersEditAccount.java
@@ -2,7 +2,10 @@ package com.lazarus.farmersmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class CustomersEditAccount extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class CustomersEditAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customers_edit_account);
+    }
+
+    public void fOnClickSave(View view) {
+        Toast.makeText(getBaseContext(),"Changes saved. Login again",Toast.LENGTH_LONG).show();
+        Intent myintent= new Intent(getBaseContext(),CustomerHome.class);
+        startActivity(myintent);
     }
 }
