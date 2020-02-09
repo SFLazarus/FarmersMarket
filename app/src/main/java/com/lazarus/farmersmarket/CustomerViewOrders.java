31645124f72c2ff7bd46fd66b8a99c149d0974c0
Owen Mellema <owenmllm@gmail.com>
2020-02-09 00:30:13 -0600

    Create Order Viewing

---
+++ app/src/main/java/com/lazarus/farmersmarket/CustomerViewOrders.java
@@ -0,0 +1,14 @@
+package com.lazarus.farmersmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerViewOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_view_orders);
+    }
+}
---
+++ app/src/main/java/com/lazarus/farmersmarket/FarmerViewOrders.java
@@ -0,0 +1,14 @@
+package com.lazarus.farmersmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmerViewOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_view_orders);
+    }
+}
